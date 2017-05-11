4cb7191ee3921c40fd5707c21fd0b585236c457c
bobmosh <ferdinand.goeldner@uni-ulm.de>
2017-05-11 10:11:13 +0200

    Initial Commit

---
+++ Focal/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Focal
+//
+//  Created by Ferdinand Göldner on 11.05.17.
+//  Copyright © 2017 Ferdinand Göldner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
